0c7e361528b944a603bb1a7d74aaca3d7564855c
Matěj Grabovský <mgrabovsky@yahoo.com>
2014-07-02 18:20:48 +0200

    HW1: Add a malfunctioning implementation of 4-peg towers

--- hw1/HanoiTowers.hs
+++ hw1/HanoiTowers.hs
@@ -12,3 +12,11 @@ hanoi n from to temp = (hanoi (n - 1) from temp to)
                        ++ [(from, to)]
                        ++ (hanoi (n - 1) temp to from)
 
+-- @FIXME: Only "works" for 0 < n < 4
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 0 _ _ _ _ = []
+hanoi4 1 from to _ _ = [(from, to)]
+hanoi4 n from to temp1 temp2 = (hanoi4 (n - 2) from temp1 temp2 to)
+    ++ [(from, temp2), (from, to), (temp2, to)]
+    ++ (hanoi4 (n - 2) temp1 to temp2 from)
+
--- hw1/testHanoiTowers.hs
+++ hw1/testHanoiTowers.hs
@@ -10,9 +10,19 @@ hanoiTest2 = TestCase $
     [("a","c"), ("a","b"), ("c","b"), ("a","c"), ("b","a"), ("b","c"), ("a","c")] @=?
     (hanoi 3 "a" "c" "b")
 
+hanoi4Test1, hanoi4Test2 :: Test
+hanoi4Test1 = TestCase $
+    [("A","B"), ("A","C"), ("A","D"), ("C","D"), ("B","D")] @=?
+    (hanoi4 3 "A" "D" "B" "C")
+hanoi4Test2 = TestCase $ 129 @=? (length $ hanoi4 15 "A" "D" "B" "C")
+
 tests :: Test
-tests = TestList [ TestLabel "2-disc case" hanoiTest1
-                 , TestLabel "3-disc case" hanoiTest2 ]
+tests = TestList [ TestLabel "3-peg towers" $
+                    TestList [ TestLabel "2-disc case" hanoiTest1
+                             , TestLabel "3-disc case" hanoiTest2 ]
+                 , TestLabel "4-peg towers" $
+                    TestList [ TestLabel "3-disc case" hanoi4Test1
+                             , TestLabel "15-disc case length" hanoi4Test2 ] ]
 
 main :: IO ()
 main = runTestTT tests >> return ()
